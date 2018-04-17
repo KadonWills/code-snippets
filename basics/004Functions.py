55f76ec775e422b2e813e970216b922711f55065
cnmoise <claudiu.moise@gmail.com>
2018-04-17 06:37:45 -0500

    Uploaded 2 more examples

---
+++ basics/004Functions.py
@@ -0,0 +1,20 @@
+# https://sagecode.net/python-data/
+
+v = 100    #declare an integer variable
+r = 100.25 #declare a float variable
+
+# Strings can be declared with " or '
+s1 = "this is a string"
+s2 = 'this is a string'
+
+# Define a list of elements
+my_list = [1,2,3,4,5]
+
+# Define a tuple of elements
+my_tuple = (1,2,3,4,5)
+
+# Define a set of elements
+my_set = {1,2,3,4,5}
+
+# Define a dictionary
+my_dic = {"a":1,"b":2,"c":3}
