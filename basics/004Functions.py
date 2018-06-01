7f376190d9c6247aa4068546a806ecc570cb3153
codercommand <codercommand@gmail.com>
2018-06-01 10:57:09 +0100

    functions
    I've updated Functions to match https://sagecode.net/python-function/

--- basics/004Functions.py
+++ basics/004Functions.py
@@ -1,20 +1,22 @@
-# https://sagecode.net/python-data/
+#fibonacci sequence
+def fib(n):
+  """Print a Fibonacci series up to n."""
+  a, b = 0, 1
+  while a < n:
+    print(a, end=' ')
+    a, b = b, a+b
+  print()
+ 
+fib(2000)
 
-v = 100    #declare an integer variable
-r = 100.25 #declare a float variable
 
-# Strings can be declared with " or '
-s1 = "this is a string"
-s2 = 'this is a string'
-
-# Define a list of elements
-my_list = [1,2,3,4,5]
-
-# Define a tuple of elements
-my_tuple = (1,2,3,4,5)
-
-# Define a set of elements
-my_set = {1,2,3,4,5}
-
-# Define a dictionary
-my_dic = {"a":1,"b":2,"c":3}
+#Calculate factorial of n
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n*factorial(n-1)
+  
+#Call factorial function and capture result
+result = factorial(5)
+print(result)
